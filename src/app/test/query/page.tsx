fba1a7537a5914f6bab2f9a4be672a4be27056d4
lilliputten <lilliputten@gmail.com>
2025-09-10 21:30:18 +0300

    Issue #1: Added test query page layout & logic.

---
+++ src/app/test/query/page.tsx
@@ -0,0 +1,26 @@
+import { constructMetadata } from '@/lib/constructMetadata';
+import { cn } from '@/lib/utils';
+import { isDev } from '@/config';
+
+import { QueryForm } from './QueryForm';
+
+export async function generateMetadata(/* { params }: TAwaitedLocaleProps */) {
+  // const { locale } = await params;
+  // const locale = defaultLanguage;
+  return constructMetadata({
+    title: 'Test Query',
+    // locale,
+  });
+}
+
+export default function TestQueryPage() {
+  return (
+    <div
+      className={cn(
+        isDev && '__TestQueryPage', // DEBUG
+      )}
+    >
+      <QueryForm />
+    </div>
+  );
+}
