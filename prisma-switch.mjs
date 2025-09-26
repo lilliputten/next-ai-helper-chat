4f05c5489f0729aafd4343a3275464805b66a037
lilliputten <lilliputten@gmail.com>
2025-09-27 01:03:05 +0300

    Issue #1: Updated prisma switch script to automatically update datatime fields in migration files.

--- prisma-switch.mjs
+++ prisma-switch.mjs
@@ -6,6 +6,7 @@
  */
 
 import fs from 'fs';
+import { glob } from 'glob';
 
 const VERCEL_URL = process.env.VERCEL_URL;
 const isVercel = !!VERCEL_URL;
@@ -30,6 +31,36 @@ if (match && requiredProvider !== foundProvider) {
   console.log('prisma-switch: Replacing...');
   const newContent = content.replace(providerRegex, '$1"' + requiredProvider + '"');
   fs.writeFileSync(prismaFile, newContent);
+  // Update migration SQL files
+  console.log('prisma-switch: Update SQL files...');
+  const migrationFiles = glob.sync('prisma/migrations/**/*.sql');
+  if (migrationFiles.length > 0) {
+    console.log('prisma-switch: Updating migration files...');
+    const isPostgres = requiredProvider === 'postgresql';
+    const postgresTimeFiled = 'TIMESTAMP(3)';
+    const sqliteTimeField = 'DATETIME';
+    const lookupStr = !isPostgres ? postgresTimeFiled : sqliteTimeField;
+    const lookupQuotedStr = lookupStr.replace(/([()])/g, '\\$1');
+    const lookupReg = new RegExp(lookupQuotedStr, 'g');
+    const replaceStr = isPostgres ? postgresTimeFiled : sqliteTimeField;
+    console.log(`prisma-switch: ${lookupStr} -> ${replaceStr}...`);
+    migrationFiles.forEach((file) => {
+      const sqlContent = fs.readFileSync(file, 'utf8');
+      const updatedContent = sqlContent.replace(lookupReg, replaceStr);
+      // if (requiredProvider === 'postgresql') {
+      //   updatedContent = ;
+      // } else {
+      //   updatedContent = sqlContent.replace(/TIMESTAMP\(3\)/g, 'DATETIME');
+      // }
+      const isChanged = updatedContent !== sqlContent;
+      console.log('prisma-switch: ' + file + ': ' + (isChanged ? 'changed' : 'unchanged'));
+      debugger;
+      if (isChanged) {
+        fs.writeFileSync(file, updatedContent);
+        console.log('prisma-switch: Updated', file);
+      }
+    });
+  }
   console.log('prisma-switch: OK');
 } else {
   console.log('prisma-switch: No changes are required, done.');
